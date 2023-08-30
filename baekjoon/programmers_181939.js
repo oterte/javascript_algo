72a17183eb5bfa31d76e58046a13dc027d9f36fe
oterte <97093479+oterte@users.noreply.github.com>
2023-08-30 16:15:14 +0900

    Update programmers_181939.js

--- baekjoon/programmers_181939.js
+++ baekjoon/programmers_181939.js
@@ -1,1 +1,12 @@
 
+function solution(a, b) {
+    var answer = 0;
+if(Number(String(a)+String(b)) > Number(String(b)+String(a))){
+    answer += Number(String(a)+String(b))
+}else if(Number(String(b)+String(a)) > Number(String(a)+String(b))){
+    answer += Number(String(b)+String(a))
+}else if(Number(String(a)+String(b)) == Number(String(b)+String(a))){
+    answer += Number(String(a)+String(b))
+}
+return answer
+}
