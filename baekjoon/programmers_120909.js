c0c86f3e7f9a9a68c34140dda22319c2fee11259
oterte <97093479+oterte@users.noreply.github.com>
2023-08-31 15:47:37 +0900

    Create programmers_120909.js

---
+++ baekjoon/programmers_120909.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    let arr =[]
+    for(let i = 1; i<=n; i++){
+        if(n % i == 0){
+            arr.push(n/i)
+        }
+    }
+    return arr.length % 2 ? 1 : 2;
+}
