56428e582f88ffb6a0ebf2f502d94c0f5d1bcc9b
oterte <97093479+oterte@users.noreply.github.com>
2023-09-05 13:45:06 +0900

    Create programmers_181934.js

---
+++ baekjoon/programmers_181934.js
@@ -0,0 +1,32 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if(ineq == ">" && eq == "="){
+        if(n>=m){
+            answer += 1;
+        }else{
+            answer += 0
+        }
+    }
+    if(ineq == "<" && eq == "="){
+        if(n<=m){
+            answer += 1;
+        }else{
+            answer += 0
+        }
+    }
+    if(ineq == ">" && eq == "!"){
+        if(n>m){
+            answer += 1;
+        }else{
+            answer += 0
+        }
+    }
+    if(ineq == "<" && eq == "!"){
+        if(n<m){
+            answer += 1;
+        }else{
+            answer += 0
+        }
+    }
+    return answer;
+}
